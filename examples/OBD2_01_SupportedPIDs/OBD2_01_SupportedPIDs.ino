71fb53026d00a5a26de731a81acc9f265ae1ce1a
Magnus Thomé <32169384+MagnusThome@users.noreply.github.com>
2023-12-09 08:18:05 +0100

    Update OBD2_01_SupportedPIDs.ino

--- examples/OBD2_01_SupportedPIDs/OBD2_01_SupportedPIDs.ino
+++ examples/OBD2_01_SupportedPIDs/OBD2_01_SupportedPIDs.ino
@@ -1,15 +1,19 @@
 // Copyright (c) Sandeep Mistry. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-#include <CAN.h> // the OBD2 library depends on the CAN library
-#include <OBD2.h>
+#include <esp32_can.h> // the ESP32_OBD2 library depends on the https://github.com/collin80/esp32_can and https://github.com/collin80/can_common CAN libraries
+#include <esp32_obd2.h>
+
+#define CAN_RX_PIN  13
+#define CAN_TX_PIN  14
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial);
 
   Serial.println(F("OBD2 Supported PIDs"));
 
+  CAN0.setCANPins((gpio_num_t)CAN_RX_PIN, (gpio_num_t)CAN_TX_PIN);
   while (true) {
     Serial.print(F("Attempting to connect to OBD2 CAN bus ... "));
 
